815fca1a0ebff156e6afdff2dc02a743da3b7add
pomdtr <achille.lacoin@gmail.com>
2024-06-20 15:44:48 +0200

    add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sqlite Explorer
+
+## Setup
+
+Rename `.env.template` to `.env` and update the values.
+
+## Commands
+
+- `deno task dev`: start development server
+- `deno task publish`: publish to jsr (ignore the warnings)
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+// import { createApp } from "jsr:@pomdtr/val-town-sqlite-explorer@0.0.3";
 import { createApp } from "./src/mod.tsx";
 
-export default createApp();
+const app = createApp({
+  token: Deno.env.get("VALTOWN_TOKEN"),
+});
+
+export default app;
